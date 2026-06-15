ed8d88d8543a8fa3086fdf37f4935be7fa0a52ac
Hisako <52304998+ImHisako@users.noreply.github.com>
2026-06-15 18:28:27 +0200

    wgf

--- src/userplugins/crashHandlerEnhanched/index.tsx
+++ src/userplugins/crashHandlerEnhanched/index.tsx
@@ -893,6 +893,7 @@ function isIgnorableDiscordRejection(error: unknown) {
     const message = getErrorMessage(error);
 
     if (message === "Aborted") return true;
+    if (message === "This gift has been redeemed already.") return true;
 
     return MESSAGE_SEND_FORBIDDEN_RE.test(message) ||
         GUILD_VANITY_FORBIDDEN_RE.test(message) ||
