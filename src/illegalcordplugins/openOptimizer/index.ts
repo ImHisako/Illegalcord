43c2a7ed47906e3aa0d38d7ad2040e34593bb10f
Hisako <52304998+ImHisako@users.noreply.github.com>
2026-08-26 19:13:04 +0200

    OpenOptimizer

--- src/illegalcordplugins/openOptimizer/index.ts
+++ src/illegalcordplugins/openOptimizer/index.ts
@@ -8,40 +8,40 @@ import { EquicordDevs } from "@utils/constants";
 import definePlugin from "@utils/types";
 
 export default definePlugin({
-	name: "OpenOptimizer",
-	description: "Ports OpenAsar's optimizer code.",
-	tags: ["Utility"],
-	authors: [EquicordDevs.seth],
-	methods: ["removeChild", "appendChild"],
-	start() {
-		for (const method of this.methods as (keyof Element)[]) {
-			this[`_${method}`] = Element.prototype[method];
-			// @ts-ignore
-			Element.prototype[method] = this.optimize(Element.prototype[method]);
-		}
-	},
-	stop() {
-		for (const method of this.methods as (keyof Element)[]) {
-			// @ts-ignore
-			Element.prototype[method] = this[`_${method}`];
-		}
-	},
+    name: "OpenOptimizer",
+    description: "Ports OpenAsar's optimizer code. ( https://openasar.dev/ ) ",
+    tags: ["Utility"],
+    authors: [EquicordDevs.seth],
+    methods: ["removeChild", "appendChild"],
+    start() {
+        for (const method of this.methods as (keyof Element)[]) {
+            this[`_${method}`] = Element.prototype[method];
+            // @ts-ignore
+            Element.prototype[method] = this.optimize(Element.prototype[method]);
+        }
+    },
+    stop() {
+        for (const method of this.methods as (keyof Element)[]) {
+            // @ts-ignore
+            Element.prototype[method] = this[`_${method}`];
+        }
+    },
 
-	// @ts-ignore
-	optimize: orig =>
-		// @ts-ignore
-		function (...args) {
-			if (
-				typeof args[0].className === "string" &&
-				(args[0].className.indexOf("activity") !== -1 ||
-					args[0].className.indexOf("subText") !== -1 ||
-					args[0].className.indexOf("botText") !== -1 ||
-					args[0].className.indexOf("clanTag") !== -1)
-			)
-				// @ts-ignore
-				return setTimeout(() => orig.apply(this, args), 100);
+    // @ts-ignore
+    optimize: orig =>
+        // @ts-ignore
+        function (...args) {
+            if (
+                typeof args[0].className === "string" &&
+                (args[0].className.indexOf("activity") !== -1 ||
+                    args[0].className.indexOf("subText") !== -1 ||
+                    args[0].className.indexOf("botText") !== -1 ||
+                    args[0].className.indexOf("clanTag") !== -1)
+            )
+                // @ts-ignore
+                return setTimeout(() => orig.apply(this, args), 100);
 
-			// @ts-ignore
-			return orig.apply(this, args);
-		},
+            // @ts-ignore
+            return orig.apply(this, args);
+        },
 });
